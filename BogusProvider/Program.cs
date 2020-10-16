21cdd030db856e3521f7d9418655a1e19adcc763
Gledson <duarte.gledson@gmail.com>
2020-10-16 15:49:03 -0300

    Added support to Docker

--- BogusProvider/Program.cs
+++ BogusProvider/Program.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Hosting;
@@ -19,12 +20,21 @@ namespace BogusProvider
                     {
                         var certificateConfig = context.Configuration.GetSection("Certificate");
                         var certFileName = certificateConfig["FileName"];
+                        if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER")))
+                        {
+                            // Insert a Docker entry on user-secrets when development in container
+                            certFileName = certificateConfig["Docker"];
+                        }
+
                         var certPassword = certificateConfig["Password"];
+                        Console.WriteLine("CertificateName: {0}", certFileName);
+                        Console.WriteLine("CertificatePassword: {0}", certPassword);
                         // Configure the Url and ports to bind to
                         // This overrides calls to UseUrls and the ASPNETCORE_URLS environment variable, but will be 
                         // overridden if you call UseIisIntegration() and host behind IIS/IIS Express
-                        options.ListenLocalhost(5002, listenOptions =>
+                        options.ListenAnyIP(5002, listenOptions =>
                         {
+                            // When development in container, use ListenAnyIP method
                             listenOptions.UseHttps(certFileName, certPassword);
                             listenOptions.Protocols = HttpProtocols.Http2;
                         });
