27aa5637a0a4be2045750d1a2eea8582ae4a3455
Gledson <duarte.gledson@gmail.com>
2020-11-08 14:14:23 -0300

    Inserted Id property to the entities

--- BogusProvider/Entities/ProductBrand.cs
+++ BogusProvider/Entities/ProductBrand.cs
@@ -2,6 +2,7 @@ namespace BogusProvider.Entities
 {
     public class ProductBrand
     {
+        public int Id { get; set; }
         public string Name { get; set; }
     }
 }
--- BogusProvider/Entities/ProductType.cs
+++ BogusProvider/Entities/ProductType.cs
@@ -2,6 +2,7 @@ namespace BogusProvider.Entities
 {
     public class ProductType
     {
+        public int Id { get; set; }
         public string Name { get; set; } 
     }
 }
