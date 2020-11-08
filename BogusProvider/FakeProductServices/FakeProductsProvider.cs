1cb56d4be16347c65bcb743a235bbe1316dd81c9
Gledson <duarte.gledson@gmail.com>
2020-11-08 14:28:56 -0300

    Added documentation to the fake product provider service

--- BogusProvider/FakeProductServices/FakeProductsProvider.cs
+++ BogusProvider/FakeProductServices/FakeProductsProvider.cs
@@ -3,9 +3,20 @@ using BogusProvider.Entities;
 
 namespace BogusProvider.FakeProductServices
 {
+    /// <summary>
+    /// Provides a fake product set
+    /// </summary>
     public class FakeProductsProvider : IFakeProductsProvider
     {
-        public Product GenerateFakeProducts()
+        /// <summary>
+        /// Generate a fake product set based on some entities types
+        /// </summary>
+        /// <param name="maxProductBrandId">the max limit number used to generate a random number
+        ///     which will be used to feed the Id property of the ProductBrand navigational property</param>
+        /// <param name="maxProductTypeId">the max limit number used to generate a random number
+        ///     which will be used to feed the Id property of the ProductType navigational property</param>
+        /// <returns></returns>
+        public Product GenerateFakeProducts(int? maxProductBrandId, int? maxProductTypeId)
         {
             var productType = new Faker<ProductType>()
                 .RuleFor(product1 => product1.Name, faker => faker.Commerce.Categories(1)[0]);
