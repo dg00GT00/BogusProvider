2eda7ae01df70c048331c8486598df66f85f7ce2
Gledson <duarte.gledson@gmail.com>
2020-11-10 16:02:46 -0300

    Added user entities to fake user generation

---
+++ BogusProvider/Entities/Identity/Address.cs
@@ -0,0 +1,12 @@
+namespace BogusProvider.Entities.Identity
+{
+    public class Address
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
---
+++ BogusProvider/Entities/Identity/AppUser.cs
@@ -0,0 +1,8 @@
+namespace BogusProvider.Entities.Identity
+{
+    public class AppUser
+    {
+        public string Email { get; set; }
+        public Address Address { get; set; }
+    }
+}
